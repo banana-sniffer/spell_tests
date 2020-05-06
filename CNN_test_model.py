cd4280843773aee092690838d3dfb3ff9c9d7aca
William Tai <williamt@berkeley.edu>
2020-05-06 11:44:21 -0700

    New testing files

--- CNN_test_model.py
+++ CNN_test_model.py
@@ -1,12 +1,30 @@
+# Loading the saved model
 from keras.models import load_model
-
 classifier = load_model('cats_vs_dogs.h5')
 
+# Getting all images in the new test folder
+from os import listdir
+from os.path import isfile, join
+onlyfiles = [f for f in listdir("dataset/single_prediction") if isfile(join("dataset/single_prediction", f))]
+
+# Testing all the images 
 import numpy as np
 from keras.preprocessing import image
-test_image = image.load_img("dataset/single_prediction/cat_or_dog_1.jpg",target_size=(64,64))
-test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image,axis=0)
-result = classifier.predict(test_image)
+results = []
+for img in onlyfiles:
+	if (img.startswith('.')):
+		continue
+	test_image = image.load_img("dataset/single_prediction/" + img,target_size=(64,64))
+	test_image = image.img_to_array(test_image)
+	test_image = np.expand_dims(test_image,axis=0)
+	result = classifier.predict(test_image)
+	if (result[0][0] == 0):
+		results.extend([img+":cat"])
+	else:
+		results.extend([img+":dog"])
+
+for i in range(len(results)):
+	print(results[i])
+
 # mapping between cats and dogs
-training_set.class_indices # {cats:0, dogs:1}
+# training_set.class_indices # {cats:0, dogs:1}
