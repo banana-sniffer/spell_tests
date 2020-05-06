46d6d15d34114ad115a2ae6894ea94071761c66b
William Tai <williamt@berkeley.edu>
2020-05-06 00:36:07 -0700

    its classifier not model

--- CNN_build.py
+++ CNN_build.py
@@ -9,7 +9,8 @@ from keras.layers import Dense
 
 # Getting the dataset from spell
 import sys
-filename = sys.argv[1]
+uploaded_training_set = sys.argv[1]
+uploaded_test_set = sys.argv[2]
 
 # Initializing the CNN
 classifier = Sequential()
@@ -45,13 +46,13 @@ train_datagen = ImageDataGenerator(
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 training_set = train_datagen.flow_from_directory(
-        'dataset/training_set',
+        uploaded_training_set,
         target_size=(64,64),
         batch_size=32,
         class_mode='binary')
 
 test_set = test_datagen.flow_from_directory(
-        'dataset/test_set',
+        uploaded_test_set,
         target_size=(64,64),
         batch_size=32,
         class_mode='binary')
@@ -59,7 +60,7 @@ test_set = test_datagen.flow_from_directory(
 classifier.fit_generator(
         training_set,
         steps_per_epoch=8000,
-        epochs=25,
+        epochs=8,
         validation_data=test_set,
         validation_steps=2000)
 
@@ -75,7 +76,7 @@ classifier.fit_generator(
 
 # Saving the model that we trained
 from keras.models import load_model
-model.save('cats_vs_dogs.h5')
+classifier.save('cats_vs_dogs.h5')
 
 
 
