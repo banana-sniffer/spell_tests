1c034a658d58a2376e82e4bb31c7f4fdab7706a6
William Tai <williamt@berkeley.edu>
2020-05-13 22:17:05 -0700

    MAGIC time

---
+++ MAGIC_Telescope/MAGIC_telescope.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('magic04.csv')
+X = dataset.iloc[:, 0:10].values
+y = dataset.iloc[:, 10].values
+
+# gamma = 1, background = 0
+for i in range(len(X[:,0])):
+	if X[:,0][i] == "g":
+		X[:,0][i] = 1
+	else:
+		X[:,0][i] = 0
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+classifier = Sequential()
+classifier.add(Dense(units = 10, kernel_initializer = 'uniform', activation = 'relu', input_dim = 10))
+classifier.add(Dense(units = 10, kernel_initializer = 'uniform', activation = 'relu'))
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+
+
---
+++ heart.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('heart.csv')
+X = dataset.iloc[:, 0:13].values
+y = dataset.iloc[:, 13].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+classifier = Sequential()
+classifier.add(Dense(units = 10, kernel_initializer = 'uniform', activation = 'relu', input_dim = 13))
+classifier.add(Dense(units = 10, kernel_initializer = 'uniform', activation = 'relu'))
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+
+
