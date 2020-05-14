160f94b88811e2d41cf48d97e8656083a224b321
William Tai <williamt@berkeley.edu>
2020-05-13 22:47:30 -0700

    true and false

--- MAGIC_Telescope/MAGIC_telescope.py
+++ MAGIC_Telescope/MAGIC_telescope.py
@@ -7,12 +7,12 @@ dataset = pd.read_csv('magic04.csv')
 X = dataset.iloc[:, 0:10].values
 y = dataset.iloc[:, 10].values
 
-# gamma = 1, background = 0
+# gamma = True, background = False
 for i in range(len(y)):
 	if y[i] == "g":
-		y[i] = 1
+		y[i] = True
 	else:
-		y[i] = 0
+		y[i] = False
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
@@ -27,8 +27,8 @@ from keras.models import Sequential
 from keras.layers import Dense
 
 classifier = Sequential()
-classifier.add(Dense(units = 10, kernel_initializer = 'uniform', activation = 'relu', input_dim = 10))
-classifier.add(Dense(units = 10, kernel_initializer = 'uniform', activation = 'relu'))
+classifier.add(Dense(units = 7, kernel_initializer = 'uniform', activation = 'relu', input_dim = 10))
+classifier.add(Dense(units = 7, kernel_initializer = 'uniform', activation = 'relu'))
 classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
@@ -41,7 +41,7 @@ classifier.save('MAGIC_Telescope.h5')
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
-accuracy = accuracy = (cm[0][0] + cm[1][1]) / len(y_pred)
+accuracy = (cm[0][0] + cm[1][1]) / len(y_pred)
 file = open("MAGIC_Telescope.txt","w")
 file.write("Accuracy: " + str(accuracy))
 file.close()
